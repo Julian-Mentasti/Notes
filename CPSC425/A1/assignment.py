b1ac98d013ef8652b4a5e023796fc94185042a07
Julian Mentasti <julian.mentasti@gmail.com>
2018-09-19 23:41:07 -0700

    finished part 1, completing part 2

--- CPSC425/A1/assignment.py
+++ CPSC425/A1/assignment.py
@@ -16,39 +16,60 @@ def boxfilter(n):
 
 
 """
-Returns a 1d gaussian array 
+Returns a 1d gaussian array with length (6*sigma) rounded up to the next odd integer
 """
 def gauss1d(sigma):
+    assert (sigma>=0), "Sigma cannot be smaller or equal to zero."
+    sigma = float(sigma)
     length = 6*sigma
     if (length%2)== 0:
-        array_length = length + 1
-    else:
-        array_length = length
-    array = np.linspace(-(length/2),(length/2),array_length) *sigma
+        length = length + 1
 
-    #gauss = lambda t: t**(-(t^2)/(2*signal^2))
-    #scale = lambda x: (-(x**2)/(2*sigma**2))
-    scale = np.exp( array^2 / (2*sigma^2))
-
-    return scale
+    edges = int(length)/2
+    arange = np.arange(-edges, edges+1)
+    result = np.exp(-arange**2/(2*sigma**2))
+    return result
 
 """
-def gauss2d(sigma):
-    1d = gauss1d(sigma)
-    two_dim = one_dim[np.newaxis]
-    2d_trans = two_dim.transpose()
-    result = signal.convolve2d(two_dim, 2d_trans)
-    return result
-    """
-
+Returns a 2d gaussian filter for a given value of sigma. 
+"""
 def gauss2d(sigma):
     oneDim = gauss1d(sigma)
     twoDim = oneDim[np.newaxis]
     twoDimT = twoDim.transpose()
     result = signal.convolve2d(twoDim, twoDimT)
+    result /= np.sum(result)
     return result
 
-def gauss(sigma=1):
-    n = sigma * 6
-    r = range(-int(n/2),int(n/2)+1)
-    return [1 / (sigma * sqrt(2*pi)) * exp(-float(x)**2/(2*sigma**2)) for x in r]
+"""
+Applies a gaussian convolution to a 2d array and returns the result
+"""
+def gaussconvolve2d(array,sigma):
+    filter = gauss2d(sigma)
+    result = signal.convolve2d(array,filter,'same')
+    return result
+
+"""
+Creates a hybrid image of sImg1 and SImg2, using the provided sigma for the filter, and saves it in file_name
+"""
+"""
+def createHybrid(sImg1, sImg2, file_name, sigma):
+    im1 = Image.open(sImg1)
+    im2 = Image.open(sImg2)
+  """
+
+"""
+Returns a blured image array of the provided image using the provided sigma
+"""
+def blurImage(im, sigma):
+    #red, green, blue = im.split()
+    red, green, blue = np.split(im, 3, axis=2)
+    red = gaussconvolve2d(red,sigma) # Red
+    green = gaussconvolve2d(green,sigma) # Green
+    blue = gaussconvolve2d(blue,sigma) # Blue
+
+    #nim = Image.merge("RGB", [red, green, blue])
+    nim = np.concatenate((red, green, blue), axis=2)
+
+    return nim
+    
